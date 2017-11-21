5301dd9163e2a753c05815d77a7cbbb958d0f851
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-21 16:37:28 -0500

    Converting a string to an int using the Convert function

--- TypeConversion/TypeConversion/Program.cs
+++ TypeConversion/TypeConversion/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TypeConversion
 {
@@ -10,9 +6,9 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            int i = 1;
-            byte b = (byte) i;
-            Console.WriteLine(b);
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
         }
     }
 }
