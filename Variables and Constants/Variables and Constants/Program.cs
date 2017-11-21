cff128eca8dda5b1d0cbac5a6c0496fb35fa2d41
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-20 20:03:55 -0500

    test printing of min and max value for bytes and floats

--- Variables and Constants/Variables and Constants/Program.cs
+++ Variables and Constants/Variables and Constants/Program.cs
@@ -6,18 +6,8 @@ namespace Variables_and_Constants
     {
         static void Main(string[] args)
         {
-            var number = 2;
-            var count = 10;
-            var totalPrice = 20.95f;
-            var character = 'A';
-            var firstName = "Dennis";
-            var isWorking = true;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         }
     }
 }
