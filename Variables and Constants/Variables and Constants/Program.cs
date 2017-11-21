77f2a0f25b61234dcb8c9c4e38c3b7eca341717a
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-20 19:54:41 -0500

    Variables and Constants lesson

---
+++ Variables and Constants/Variables and Constants/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables_and_Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Dennis";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
