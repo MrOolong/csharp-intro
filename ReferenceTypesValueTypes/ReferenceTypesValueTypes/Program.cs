d8d72f71efd8991376c33eb106c0417e5bbf2920
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-23 20:59:16 -0500

    Reference types and value types

---
+++ ReferenceTypesValueTypes/ReferenceTypesValueTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypesValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+        }
+    }
+}
