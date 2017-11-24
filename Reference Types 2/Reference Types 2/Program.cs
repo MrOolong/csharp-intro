a77fc3e0efaa3a430445d1ee3a28c17b8bd14aa0
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-24 17:53:46 -0500

    reference types take 2

---
+++ Reference Types 2/Reference Types 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_Types_2
+{
+    public class Person
+    {
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
