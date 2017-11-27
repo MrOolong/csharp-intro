d9617391a27aeefa2b8a7d31c61e1178ebc4ba12
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-26 23:18:21 -0500

    Control Flow - Exercise 1

---
+++ ControlFlow-Exercise1/ControlFlow-Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user to enter number between 1-10
+            int number;
+            Console.WriteLine("Enter a valid number between 1-10");
+            number = Convert.ToInt32(Console.ReadLine());
+            // if valid number, display "valid"
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
