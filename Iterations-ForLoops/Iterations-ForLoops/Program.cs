523c3dcd263f0b2a0fb7cd35f826470f38be3dae
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-27 18:00:28 -0500

    iterations for & for each

--- Iterations-ForLoops/Iterations-ForLoops/Program.cs
+++ Iterations-ForLoops/Iterations-ForLoops/Program.cs
@@ -10,6 +10,21 @@ namespace Iterations_ForLoops
     {
         static void Main(string[] args)
         {
+            for (var i = 1; i <= 10; i ++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
