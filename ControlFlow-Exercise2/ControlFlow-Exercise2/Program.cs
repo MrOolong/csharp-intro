2c2b83579a16a540c6291233d35ca6f7807ba3bd
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-26 23:34:40 -0500

    Control Flow Exercise 2 & 3

---
+++ ControlFlow-Exercise2/ControlFlow-Exercise2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program which takes two numbers from the console and displays the maximum of the two.
+        }
+    }
+}
---
+++ ControlFlow-Exercise3/ControlFlow-Exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width and height of an image");
+            int width, height; 
+
+            if ( width > height)
+            {
+                Console.WriteLine("The image is landscape");
+            }
+            else
+            {
+                Console.WriteLine("The image is portrait");
+            }
+
+        }
+    }
+}
