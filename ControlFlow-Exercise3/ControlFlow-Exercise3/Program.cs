00a7b8704aec6097f510f0eccabd7de09c640f73
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-27 16:43:23 -0500

    exercise 3 - still need multiple input

--- ControlFlow-Exercise3/ControlFlow-Exercise3/Program.cs
+++ ControlFlow-Exercise3/ControlFlow-Exercise3/Program.cs
@@ -11,7 +11,9 @@ namespace ControlFlow_Exercise3
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the width and height of an image");
-            int width, height; 
+            int width, height;
+            //var numbers = new int[2];
+            //numbers = Convert.ToInt32(Console.ReadLine(width, height));
 
             if ( width > height)
             {
