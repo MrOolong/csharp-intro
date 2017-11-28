ab611f2d6476858268992449ed01ff7693acbae9
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-28 18:48:50 -0500

    Additional array examples - rectangular and jagged

---
+++ MoreArrays/MoreArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            // length
+            Console.WriteLine("Length: " + numbers.Length);
+        }
+    }
+}
