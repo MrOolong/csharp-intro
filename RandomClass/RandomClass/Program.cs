854778e9c2175446df76c9dba592646f48a286ac
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-27 20:34:39 -0500

    random class lesson to demonstrate random number and letter generators

---
+++ RandomClass/RandomClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            const int passwordlength = 10;
+
+            var buffer = new char[passwordlength];
+            for (var i = 0; i < passwordlength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+
+            // print the ascii digit for an letter
+            //Console.WriteLine((int) 'a');
+        }
+    }
+}
