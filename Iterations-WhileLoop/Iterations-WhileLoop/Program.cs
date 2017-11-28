fac66edcce3dd25989f8bcaa1abfafd3dccd841e
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-27 20:04:46 -0500

    initial commit

---
+++ Iterations-WhileLoop/Iterations-WhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations_WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+
+                i++;
+            }
+        }
+    }
+}
