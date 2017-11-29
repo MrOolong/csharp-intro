d92714b7fa7be69a05f934656ca9ae21578387fc
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-28 19:58:38 -0500

    DateTime is a namespace but is used as a type error

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -10,7 +10,13 @@ namespace DateTime
     {
         static void Main(string[] args)
         {
+            //DateTime
             var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
         }
     }
 }
