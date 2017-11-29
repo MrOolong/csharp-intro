db98778920938fbadd655331df290ee578028529
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-28 19:40:47 -0500

    DateTime initial commit

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+        }
+    }
+}
