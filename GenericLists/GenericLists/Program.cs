a1880736cfeb29c64d43c4e88d72d082d1a8f198
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-28 19:12:18 -0500

    Lists have add methods. You can add as many items as you'd like

---
+++ GenericLists/GenericLists/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace GenericLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+        }
+    }
+}
