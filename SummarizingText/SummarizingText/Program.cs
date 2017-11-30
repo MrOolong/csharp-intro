dd9e2267f4dde1ad74811e97db167ae4d7107f7d
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-30 09:19:02 -0500

    using length and lists to produce a portion of text from a larger sentence

---
+++ SummarizingText/SummarizingText/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            const int maxLength = 20;
+
+            if (sentence.Length < maxLength)
+                Console.WriteLine(sentence);
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+                // sentence.Substring(0, maxLength);
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                        break;
+                }
+
+                var summary = String.Join(" ", summaryWords) + "...";
+                Console.WriteLine(summary);
+            }
+        }
+    }
+}
