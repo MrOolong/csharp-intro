4cc594c828f3db3fd3c3c80627ea54e943b6c064
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-11-29 23:06:37 -0500

    two ways to split a string

--- Text-Strings/Text-Strings/Program.cs
+++ Text-Strings/Text-Strings/Program.cs
@@ -12,6 +12,17 @@ namespace Text_Strings
         {
             var fullName = "Dennis D ";
             Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
         }
     }
 }
