b5b885662350cf40c020149e7a1d5c26f142b698
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-02 11:38:05 -0500

    chaining sub-methods from the string builder method

--- StringBuilderApp/StringBuilderApp/Program.cs
+++ StringBuilderApp/StringBuilderApp/Program.cs
@@ -10,9 +10,20 @@ namespace StringBuilderApp
     {
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            builder.Append('-', 10);
+            var builder = new StringBuilder("Hello World");
+            builder
+                .Append('-', 10)
+            // insert a new line
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
             Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
         }
     }
 }
