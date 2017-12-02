5a8f6f8fc4114b89e993a3874246efa4e26ec606
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-02 11:18:24 -0500

    string builder initial commit

---
+++ StringBuilderApp/StringBuilderApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10);
+            Console.WriteLine(builder);
+        }
+    }
+}
