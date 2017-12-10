cf4ea927ef848f1da23ba5e452619ebb58e1ee12
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-09 23:04:48 -0500

    successfully counts number of words within a text file

--- Section9_Exercise1/Section9_Exercise1/Program.cs
+++ Section9_Exercise1/Section9_Exercise1/Program.cs
@@ -16,17 +16,26 @@ namespace Section9_Exercise1
             string countText = File.ReadAllText(path);
             var countArray = countText.Split(' ');
 
-            Console.WriteLine(countText);
-            Console.WriteLine("There are " + countText.Length + " characters in test.txt");
+            // Print the text within test1.txt
+            Console.WriteLine("test1.txt contains the following text:");
+            Console.WriteLine("\"" + countText + "\"");
+            Console.WriteLine();
+
+            // Print the number of characters within test1.txt
+            Console.WriteLine("There are " + countText.Length + " characters in test1.txt");
+            Console.WriteLine();
+
+            // Print the number of words within test1.txt
+            Console.WriteLine("There are " + countArray.Length + " words in test1.txt");
 
             // printing indiviual word within array - now need to count it
             // Console.WriteLine(countArray[0]);
 
             // foreach loop to print each individual word
-            foreach (string word in countArray)
-            {
-                System.Console.WriteLine(word);
-            }
+            //foreach (string word in countArray)
+            //{
+            //    System.Console.WriteLine(word);
+            //}
         }
     }
 }
