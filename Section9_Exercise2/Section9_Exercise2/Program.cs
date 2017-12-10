d0e21d7e6f9a959d03228b351fc34ab7650720e4
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-09 23:26:54 -0500

    created new text file to test - character length testing

--- Section9_Exercise2/Section9_Exercise2/Program.cs
+++ Section9_Exercise2/Section9_Exercise2/Program.cs
@@ -11,31 +11,33 @@ namespace Section9_Exercise2
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\dennis\Desktop\csharp-intro\Section9_Exercise1\test1.txt";
+            string path = @"C:\Users\dennis\Desktop\csharp-intro\Section9_Exercise2\test2.txt";
 
             string countText = File.ReadAllText(path);
             var countArray = countText.Split(' ');
 
             // Print the text within test1.txt
-            Console.WriteLine("test1.txt contains the following text:");
+            Console.WriteLine("test2.txt contains the following text:");
             Console.WriteLine("\"" + countText + "\"");
             Console.WriteLine();
 
             // Print the number of characters within test1.txt
-            Console.WriteLine("There are " + countText.Length + " characters in test1.txt");
+            Console.WriteLine("There are " + countText.Length + " total characters in test2.txt");
             Console.WriteLine();
 
             // Print the number of words within test1.txt
-            Console.WriteLine("There are " + countArray.Length + " words in test1.txt");
+            //Console.WriteLine("There are " + countArray.Length + " words in test2.txt");
 
             // printing indiviual word within array - now need to count it
             // Console.WriteLine(countArray[0]);
 
-            // foreach loop to print each individual word
-            //foreach (string word in countArray)
-            //{
-            //    System.Console.WriteLine(word);
-            //}
+            //foreach loop to print each individual word
+            Console.WriteLine("The char length of each word in test2.txt is as follows:");
+            foreach (string word in countArray)
+            {
+                // word.Length displays character length - use if logic to display the longest
+                System.Console.WriteLine(word.Length);
+            }
         }
     }
 }
