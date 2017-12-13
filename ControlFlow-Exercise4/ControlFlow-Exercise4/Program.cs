709d86c67e816fea4144b95cb35dc7749378033f
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-13 08:59:52 -0500

    Control Flow - Exercise 4 - Initial Commit

---
+++ ControlFlow-Exercise4/ControlFlow-Exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow_Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program which takes two numbers from the console and displays the maximum of the two.
+            Console.WriteLine("Please enter a number");
+            var userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a 2nd number");
+            var userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput1 > userInput2)
+            {
+                Console.WriteLine(userInput1 + " is the larger number");
+            }
+            else
+            {
+                Console.WriteLine(userInput2 + " is the larger number");
+            }
+        }
+    }
+}
