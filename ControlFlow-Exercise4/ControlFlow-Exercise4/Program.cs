ba80f7b216464716ae4ab68e71d1b59c0b6c2fae
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-13 10:02:36 -0500

    Added second difference calculation using modulo

--- ControlFlow-Exercise4/ControlFlow-Exercise4/Program.cs
+++ ControlFlow-Exercise4/ControlFlow-Exercise4/Program.cs
@@ -13,16 +13,20 @@ namespace ControlFlow_Exercise4
             // Write a program which takes two numbers from the console and displays the maximum of the two.
             Console.WriteLine("Please enter the speed limit");
             var userInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
             Console.WriteLine("Please enter the speed of a car");
             var userInput2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
             // calculate the difference between the speed of the car and the speed limit
             int difference = (userInput2 - userInput1);
+            int difference1 = (userInput2 % userInput1);
 
             if (userInput2 > userInput1)
             {
                 Console.WriteLine(difference);
+                Console.WriteLine(difference1);
             }
             else
             {
