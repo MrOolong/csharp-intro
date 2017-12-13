a2606ff549c1b49a5dac090ebd689fe8f07a79a5
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-13 08:53:02 -0500

    Completed Control Flow - Exercise 2(section 5)

--- ControlFlow-Exercise2/ControlFlow-Exercise2/Program.cs
+++ ControlFlow-Exercise2/ControlFlow-Exercise2/Program.cs
@@ -11,6 +11,20 @@ namespace ControlFlow_Exercise2
         static void Main(string[] args)
         {
             // Write a program which takes two numbers from the console and displays the maximum of the two.
+            Console.WriteLine("Please enter a number");
+            var userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a 2nd number");
+            var userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            if ( userInput1 > userInput2)
+            {
+                Console.WriteLine(userInput1 + " is the larger number");
+            }
+            else
+            {
+                Console.WriteLine(userInput2 + " is the larger number");
+            }
         }
     }
 }
