e43a5af1e4fda86eec27b81c05f489a1bf5673c2
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-14 16:47:45 -0500

    section 8 - exercise 1 - initial commit

---
+++ Section8_Exercise1/Section8_Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
