f4f1e50834d38f27a35c0e82e22c45f4f3fd189f
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-14 20:53:06 -0500

    added nested for loops to loop through the user input

--- Section8_Exercise1/Section8_Exercise1/Program.cs
+++ Section8_Exercise1/Section8_Exercise1/Program.cs
@@ -14,10 +14,17 @@ namespace Section8_Exercise1
             Console.WriteLine("Please enter a few numbers separated by hypens");
             var userInput1 = Console.ReadLine();
             var userString = userInput1.Split('-');
+            //int userStringInt = Convert.ToInt32(userString);
 
-            Console.WriteLine(userString[0]);
+            // Console.WriteLine(userString[0]);
 
-            
+            for (var i = 0; i < userString.Length; i ++)
+            {
+                for (var j = 0; j < userString.Length; j++)
+                {
+
+                }
+            }
 
             //if (true)
             //{
