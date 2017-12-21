3b00d3bbfcdd6112a03d8036091fb80d39acfb7f
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-21 13:17:18 -0500

    created basic logic

--- Section5_Exercise1/Section5_Exercise1/Program.cs
+++ Section5_Exercise1/Section5_Exercise1/Program.cs
@@ -10,6 +10,20 @@ namespace Section5_Exercise1
     {
         static void Main(string[] args)
         {
+            int min = 1;
+            int max = 100;
+            int counter = 0;
+            int divisor = 3;
+
+            for (int i = min; i < max; i++)
+            {
+                if (i / divisor)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("X amount of numbers are divisible by 3");
         }
     }
 }
