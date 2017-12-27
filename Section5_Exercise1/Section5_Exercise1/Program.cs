1fdb98fd5193b49a8df2550fe7adb8916a153b57
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-26 21:35:53 -0500

    updated comments

--- Section5_Exercise1/Section5_Exercise1/Program.cs
+++ Section5_Exercise1/Section5_Exercise1/Program.cs
@@ -10,6 +10,10 @@ namespace Section5_Exercise1
     {
         static void Main(string[] args)
         {
+            // Write a program to count how many numbers
+            // between 1 and 100 are divisible by 3 with no remainder.
+            // Display the count on the console.
+
             int min = 1;
             int max = 100;
             int counter = 0;
@@ -19,8 +23,9 @@ namespace Section5_Exercise1
             {
                 if ()
                 {
-                    counter++;
+                    
                 }
+                counter++;
             }
 
             Console.WriteLine(counter + " Numbers are divisible by 3");
